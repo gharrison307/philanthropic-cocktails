1fd823bd773d5c57094a7d8a19b78676fe6580e8
Alexandru S <alexandru11723@gmail.com>
2023-02-24 14:39:56 -0700

    fetchCocktails function added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,54 @@
 // Variable Declarations
 
-function fetchCocktails(){
 
+//functions
 
+/*
+    Fuction to fetch and return the cocktail data
+    Accepting parameters:
+    - endpoint : String - should accept one of the options: [ search, filter, random or list ]
+    - parameter : String - should accept the options [s, i, f, iid, g, a, c]
+    - value : String - the filer of search value to pass with parameter
+*/
+async function fetchCocktails(endpoint, parameter, value) {
+    var baseURL = "https://www.thecocktaildb.com/api/json/v1/1/";
+    var requestURL;
+
+    if(endpoint === "random"){
+        requestURL = baseURL + "random.php";
+    }else{
+        requestURL = baseURL + endpoint + ".php?" + parameter + "=" + value;
+    }
+
+    return await fetch(requestURL);
 }
 
-function fetchDonatios(){
+//example of how to call fetchCocktails
+fetchCocktails("filter","i", "Gin")
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+        console.log(data);
+    });
 
+/* 
+    Function to fetch the donation information
+    Accepting parameters:
+    - cause : string
 
+    Example: fetchDonations("climate")
+*/
+function fetchDonatios(cause) {
+    
 }
 
-function saveLocalData(){
+/* 
+    Save the data to local storage
+*/
+function saveLocalData() {}
 
-
-}
-
-function displayLocalData(){
-
-
-}
-
+/*
+    Get data from local storage
+*/
+function displayLocalData() {}
