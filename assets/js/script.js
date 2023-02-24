319f98390be4f6f0de364c745cc29ae7ffb481be
George Harrison <g.a.harrison91@gmail.com>
2023-02-24 12:03:50 -0700

    formated sidebar, added code to produce buttons with drink history in sidebar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,37 @@
 // Variable Declarations
 
-function fetchCocktails(){
+var previousCocktails = JSON.parse(localStorage.getItem(" /KEY/  "));
+var savedDrinks = document.getElementById("savedDrinks");
+var producedDrink = /reference pointing to drink suggestion/;
 
+function fetchCocktails() {}
 
+function fetchDonatios() {}
+
+function saveLocalData() {}
+
+function displayLocalData() {}
+
+function drinkHistory() {
+  var searchResult = producedDrink.value.trim().toLowerCase();
+  console.log(previousCocktails.indexOf(searchResult));
+  console.log(searchResult);
+
+  if (searchResult === "") {
+    return;
+  } else if (previousCocktails.indexOf(searchResult) >= 0) {
+    return;
+  } else {
+    previousCocktails.push(searchResult);
+    localStorage.setItem(" /KEY/ ", JSON.stringify(previousCocktails));
+    previousCocktails = JSON.parse(localStorage.getItem(" /KEY/ "));
+    savedDrinks.textContent = "";
+
+    for (i = 0; i < previousCocktails.length; i++) {
+      var addedDrink = document.createElement("button");
+      addedDrink.textContent = previousCocktails[i];
+      addedDrink.className = "savedDrinks";
+      savedDrinks.appendChild(addedDrink);
+    }
+  }
 }
-
-function fetchDonatios(){
-
-
-}
-
-function saveLocalData(){
-
-
-}
-
-function displayLocalData(){
-
-
-}
-
